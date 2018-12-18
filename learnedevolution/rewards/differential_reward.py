1abd82cbbc588dad39e94c0b3f395c5e3ee51b82
Erik Meulman <realtwister@gmail.com>
2018-12-18 14:16:41 +0100

    Correct differential reward factor to reflect paper

--- learnedevolution/rewards/differential_reward.py
+++ learnedevolution/rewards/differential_reward.py
@@ -3,32 +3,26 @@ import numpy as np;
 from .reward import Reward;
 
 class DifferentialReward(Reward):
-    def __init__(self, epsilon = 1e-20):
-        self.epsilon = epsilon;
+    def __init__(self,
+        epsilon):
+        self.epsilon = epsilon
+
     def _reset(self):
         self._step = -1;
         self._fitness_diff = 0;
         self._mean_fitness = 0;
         self._prev_fitness_diff = 1;
         self._prev_mean_fitness = 0;
-        self._max=-float('Inf');
+        self._max = -float('Inf');
 
-    def __call__(self,population,  fitness):
+    def __call__(self, population,  fitness):
         self._step += 1;
-        if np.max(fitness)> self._max:
-            self._max= np.max(fitness);
+        self._max = max(np.max(fitness), self._max);
         self._prev_mean_fitness = self._mean_fitness;
         self._mean_fitness = np.mean(fitness);
-        factor =max(self._max-self._mean_fitness, self.epsilon)
+        factor = max(self._max - self._prev_mean_fitness, self.epsilon)
         reward = (self._mean_fitness-self._prev_mean_fitness)/factor if self._step > 1 else 0;
         return reward;
-        self._fitness_diff = self._mean_fitness-self._prev_mean_fitness;
-
-        reward = (max(np.abs(self._prev_fitness_diff),1e-6)/self._fitness_diff ) if self._step > 1 else 0;
-        reward = np.clip(reward, -5,5);
-        self._prev_fitness_diff = self._fitness_diff;
-        self._reward = reward;
-        return reward^3+reward;
 
     @classmethod
     def _get_kwargs(cls, config, key = ""):
@@ -36,6 +30,5 @@ class DifferentialReward(Reward):
             'epsilon'
         )
         cls._config_defaults(
-            epsilon = 1e-20
         )
         return super()._get_kwargs(config, key = key);
